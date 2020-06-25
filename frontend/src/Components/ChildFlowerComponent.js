252be4686b619814e1e17a17477db8451dd1fa0e
Quantaxer <phudel@uoguelph.ca>
2020-06-25 18:34:50 -0400

    Format child

--- frontend/src/Components/ChildFlowerComponent.js
+++ frontend/src/Components/ChildFlowerComponent.js
@@ -3,10 +3,15 @@ import "../Styling/Container.css";
 import CustomComponent from "../customComponent.js";
 
 class ChildFlower extends CustomComponent {
-  render() {
+    render() {
     return (
         <div className="ChildFlower">
             <p>{this.props.color}</p>
+            {this.props.listOfChildren.map(child => (
+                <div>
+                    <p>{child.numericGenotype}: {child.probability}%</p>
+                </div>
+            ))}
         </div>
     )}
 }
