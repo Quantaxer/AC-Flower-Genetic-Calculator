756b963bf87124d0c5a004553ad1bb7909cd677a
Peter Hudel <peter.hudel@tulip.com>
2020-07-16 11:57:46 -0400

    Add material ui custom components: clean this up later

--- frontend/src/Components/ChildFlowerComponent.js
+++ frontend/src/Components/ChildFlowerComponent.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "../Styling/Container.css";
+import Card from "../assets/jss/Card";
+import CardBody from "../assets/jss/CardBody";
 import CustomComponent from "../customComponent.js";
 
 class ChildFlower extends CustomComponent {
-    render() {
+  render() {
     return (
-        <div className="ChildFlower">
-            <p>{this.props.color}</p>
-            {this.props.listOfChildren.map(child => (
-                <div>
-                    <p>{child.numericGenotype}: {child.probability}%</p>
-                </div>
+      <div className="ChildFlower">
+          <p>Results</p>
+        <Card style={{ width: "20rem" }}>
+          <p>{this.props.color}</p>
+          <CardBody>
+            {this.props.listOfChildren.map((child) => (
+              <div>
+                <p>
+                  {child.numericGenotype}: {child.probability}%
+                </p>
+              </div>
             ))}
-        </div>
-    )}
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
 }
 
 export default ChildFlower;
